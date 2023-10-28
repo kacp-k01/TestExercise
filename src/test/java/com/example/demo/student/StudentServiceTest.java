019393ff2076a7b2fb37a919db7c5e9ab7e2bd9c
Kacper <kacpkuc@gmail.com>
2023-10-28 22:33:54 +0200

    Test 2 Clean Up

--- src/test/java/com/example/demo/student/StudentServiceTest.java
+++ src/test/java/com/example/demo/student/StudentServiceTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
 
 class StudentServiceTest {
@@ -16,30 +15,24 @@ class StudentServiceTest {
     private StudentRepository studentRepository;
     private StudentService underTEst;
     private AutoCloseable autoCloseable;
-
     @BeforeEach
-    void setUp(){
-        autoCloseable= MockitoAnnotations.openMocks(this);
+    void setUp() {
+        autoCloseable = MockitoAnnotations.openMocks(this);
         underTEst = new StudentService(studentRepository);
     }
-
     @AfterEach
     void tearDown() throws Exception {
         autoCloseable.close();
     }
-
     @Test
     void canGetAllStudents() {
         underTEst.getAllStudents();
         verify(studentRepository).findAll();
-
     }
-
     @Test
     @Disabled
     void addStudent() {
     }
-
     @Test
     @Disabled
     void deleteStudent() {
